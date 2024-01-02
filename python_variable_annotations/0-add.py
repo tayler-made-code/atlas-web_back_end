6e43b0c82fc26ae628d219df9b759335599be190
F-O-P <tayler.coon@gmail.com>
2024-01-02 13:47:04 -0600

    task 1 pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
+""" Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float """
 
-# Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
 
 def add(a: float, b: float) -> float:
-  """
-  Returns the sum of two floats.
 
-  Parameters:
-  a (float): The first float number.
-  b (float): The second float number.
+  """" Returns the float-sum of two floats. """
 
-  Returns:
-  float: The sum of a and b.
-  """
   return a + b
