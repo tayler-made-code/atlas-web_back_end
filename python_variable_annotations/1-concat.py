2c096031c5c9a0910129eff3e154dc6115c07cab
F-O-P <tayler.coon@gmail.com>
2024-01-03 12:45:16 -0600

    task two is functional with pycodestyles

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+""" Write a type-annotated function concat that takes a string str1
+    and a string str2 as arguments and returns a concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """ Returns the concatenated string of two strings """
+
+    return str1 + str2
