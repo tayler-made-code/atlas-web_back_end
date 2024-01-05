63641169f14005bd6c25b730f1d33178ad866fa4
F-O-P <tayler.coon@gmail.com>
2024-01-05 11:42:40 -0600

    task 1 is working, gotta add that async on the def

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+""" Import wait_random from the previous python file that you've written
+    and write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random n times
+    with the specified max_delay
+
+    wait_n should return the list of all the delays (float values). The list
+    of the delays should be in ascending order without using sort() because
+    of concurrency """
+
+import importlib
+import asyncio
+from typing import List
+basic_async_syntax = importlib.import_module("0-basic_async_syntax")
+wait_random = basic_async_syntax.wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Return list of delays in ascending order """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+
+""" Import wait_random from 0-basic_async_syntax
+
+    Write a function (do not create an async function, use the regular
+    function syntax to do this) task_wait_random that takes an integer
+    max_delay and returns a asyncio.Task """
