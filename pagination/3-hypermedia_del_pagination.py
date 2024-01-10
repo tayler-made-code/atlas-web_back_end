308ffe25b30a386a78fee4259957177b94dd02e2
F-O-P <tayler.coon@gmail.com>
2024-01-10 15:53:09 -0600

    task 3 works, forgot to add dict import

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,1 +1,65 @@
 #!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ Implement a get_hyper_index method with two integer
+            arguments: index with a None default value and page_size
+            with default value of 10 """
+
+        assert type(index) == int or index is None
+        assert type(page_size) == int and page_size > 0
+
+        dataset = self.indexed_dataset()
+        assert 0 <= index < len(dataset)
+
+        data = []
+        next_index = index
+        for i in range(page_size):
+            while next_index not in dataset.keys():
+                next_index += 1
+            data.append(dataset[next_index])
+            next_index += 1
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': page_size,
+            'data': data
+        }
