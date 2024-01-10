db3ff3660505c2716bb52407138fffd841b3a634
F-O-P <tayler.coon@gmail.com>
2024-01-10 15:18:41 -0600

    task 1 base caching working and pycodestyle compliant

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+""" Create a class BasicCache that inherits from BaseCaching and
+    is a caching system """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """ Create a class BasicCache that inherits from BaseCaching and
+        is a caching system """
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
