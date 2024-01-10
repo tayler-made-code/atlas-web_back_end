6673982b4529b43a9aeb60019927d5cb156aa7af
F-O-P <tayler.coon@gmail.com>
2024-01-10 15:22:14 -0600

    added extra spacing

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,6 +5,7 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
     """ Create a class BasicCache that inherits from BaseCaching and
         is a caching system """
