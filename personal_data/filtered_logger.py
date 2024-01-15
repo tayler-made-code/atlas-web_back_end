b485b97650f3a0919d4e75ad5c0fd6408cda0fbc
F-O-P <tayler.coon@gmail.com>
2024-01-15 11:53:01 -0600

    task 2 working and pycode compliant

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,24 @@
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        frmt = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+        super(RedactingFormatter, self).__init__(frmt)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum
+        (self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
