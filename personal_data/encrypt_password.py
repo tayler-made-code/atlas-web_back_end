624711177187536dd91a749289d4f29167b1bd53
F-O-P <tayler.coon@gmail.com>
2024-01-15 15:37:40 -0600

    is_valid added

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,3 +12,8 @@ def hash_password(password: str) -> bytes:
     """ hash_password returns a salted, hashed password, which is a byte string
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is_valid returns a boolean """
+    return bcrypt.checkpw(password.encode(), hashed_password)
