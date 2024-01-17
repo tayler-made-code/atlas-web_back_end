999296643c56f69de80b711281b0e2524db64d60
F-O-P <tayler.coon@gmail.com>
2024-01-17 12:22:58 -0600

    imiplemented basicauth decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ decode base64 auth header """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64_authorization_header.decode('utf-8')
+        except Exception:
+            return None
