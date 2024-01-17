ff6825678d3a22bad599ec8fd720cb2e2e2a1d58
F-O-P <tayler.coon@gmail.com>
2024-01-17 12:21:08 -0600

    implemented basicauth extract authorization header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,18 @@ from typing import List, TypeVar
 
 class BasicAuth(Auth):
     """ basic auth class """
+
+    def __init__(self):
+        """ init """
+        pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract base64 auth header """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
