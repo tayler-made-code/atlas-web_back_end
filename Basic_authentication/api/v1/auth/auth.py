0d27c02869f5043c30920f94d535b8ba195a3a88
F-O-P <tayler.coon@gmail.com>
2024-01-17 11:46:00 -0600

    reading the task explains the task

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 
-""" BasicAuth module """
+""" Auth class """
 
 from flask import request
+from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """ Auth class """
 
+    def __init__(self):
+        """ init """
+        pass
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth """
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
+        """ require auth public method """
+        return False
 
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header """
-        if request is None or request.headers.get('Authorization') is None:
-            return None
-        return request.headers.get('Authorization')
+        """ authorization header public method """
+        return None
     
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user """
+        """ current user public method"""
         return None
