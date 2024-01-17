76f95740287c7baf846ac1f9fc2069b710c6ba76
F-O-P <tayler.coon@gmail.com>
2024-01-17 12:37:08 -0600

    implemented extract user

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,3 +42,14 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ extract user credentials """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':', 1))
