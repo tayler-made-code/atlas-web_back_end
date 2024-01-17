dfc6d763c8b1c14d14fbe280a1425e64dc5bdb4e
F-O-P <tayler.coon@gmail.com>
2024-01-17 12:35:16 -0600

    fixed decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@
 from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -34,6 +35,10 @@ class BasicAuth(Auth):
         if type(base64_authorization_header) is not str:
             return None
         try:
-            return base64_authorization_header.decode('utf-8')
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64.b64decode(base64_bytes)
+            message = message_bytes.decode('utf-8')
+            return message
         except Exception:
             return None
+
