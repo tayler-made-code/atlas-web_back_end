4d8914d32270ff33517690805038d0d0ad9757e1
F-O-P <tayler.coon@gmail.com>
2024-01-23 14:52:26 -0600

    fixed user import

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,8 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -31,10 +30,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> None:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Method that saves a new user to the database
         """
-        from user import User
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
