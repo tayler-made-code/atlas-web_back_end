2d640af0e465e424c10b77384600141c13df3067
F-O-P <tayler.coon@gmail.com>
2024-01-23 08:50:44 -0600

    fixed pycodestyling

--- Sessions_authentication/api/v1/views/users.py
+++ Sessions_authentication/api/v1/views/users.py
@@ -26,11 +26,11 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     if user_id == 'me' and request.current_user is None:
-      abort(404)
+        abort(404)
 
     if user_id == 'me' and request.current_user is not None:
-      return jsonify(request.current_user.to_json())
-    
+        return jsonify(request.current_user.to_json())
+
     if user_id is None:
         abort(404)
 
