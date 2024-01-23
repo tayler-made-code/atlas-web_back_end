0ba848e1e13a59ffda32513982f2b981f88678d5
F-O-P <tayler.coon@gmail.com>
2024-01-23 08:56:00 -0600

    fixed pycode styling

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -34,7 +34,7 @@ def before_request_func() -> str:
                                         '/api/v1/forbidden/']):
         if auth.authorization_header(request) is None:
             abort(401)
-        
+
         request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403)
