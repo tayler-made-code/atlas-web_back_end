4252770398107813d2f1b902ddbd3f57cdeb5065
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:47:25 -0600

    register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,20 +6,12 @@ from db import DB
 from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """ Method that takes in a password string arguments and returns bytes.
-    """
-    if isinstance(password, str):
-        password = password.encode('utf-8')
-
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password, salt)
-    return hashed_password
-
-
 class Auth:
     """Auth class to interact with the authentication database.
     """
+
+    def __init__(self):
+        self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
         """ Method that takes mandatory email and password string arguments
@@ -34,6 +26,13 @@ class Auth:
             user = self._db.add_user(email, hashed_password)
             return user
 
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """ Method that takes in a password string arguments and returns bytes.
+        """
+        if isinstance(password, str):
+            password = password.encode('utf-8')
 
-def __init__(self):
-    self._db = DB()
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        return hashed_password
