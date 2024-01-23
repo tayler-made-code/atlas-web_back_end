504a2b3b52f9b5083e9b77ff06782321e17600a8
F-O-P <tayler.coon@gmail.com>
2024-01-23 17:04:27 -0600

    moved hash password back

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,14 @@ from db import DB
 from user import User
 
 
+def _hash_password(password: str) -> bytes:
+    """ Method that takes in a password string arguments and returns bytes.
+    """
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    return hashed_password
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -25,12 +33,4 @@ class Auth:
             hashed_password = Auth._hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
-
-    @staticmethod
-    def _hash_password(password: str) -> bytes:
-        """ Method that takes in a password string arguments and returns bytes.
-        """
-        password_bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password_bytes, salt)
-        return hashed_password
+        
