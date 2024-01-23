25876a6fd57f925f13f2b5a8665f8efe419ca2f5
F-O-P <tayler.coon@gmail.com>
2024-01-23 12:05:47 -0600

    checking documentation

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,14 +3,6 @@
 """ Session Auth """
 
 from api.v1.auth.auth import Auth
-from api.v1.auth.session_auth import SessionAuth
-import os
-
-auth_type = os.getenv('AUTH_TYPE')
-if auth_type == 'session_auth':
-    auth = SessionAuth()
-else:
-    auth = Auth()
 
 
 class SessionAuth(Auth):
