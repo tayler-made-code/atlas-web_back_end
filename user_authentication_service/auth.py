567cf1aa9238c602c5159919448ad23fbbca4208
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:31:04 -0600

    fixed annotation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,7 +4,7 @@
 import bcrypt
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> bytes:
     """ Method that takes in a password string arguments and returns bytes.
     """
     if isinstance(password, str):
