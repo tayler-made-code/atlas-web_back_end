3ff295010cca1e782521db222822699d170ade9f
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:36:24 -0600

    implemented register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,14 +2,36 @@
 
 """ Auth module """
 import bcrypt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """ Method that takes in a password string arguments and returns bytes.
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    if isinstance(password, str):
-        password = password.encode('utf-8')
 
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password, salt)
-    return hashed_password
+    def _hash_password(password: str) -> bytes:
+        """ Method that takes in a password string arguments and returns bytes.
+        """
+        if isinstance(password, str):
+            password = password.encode('utf-8')
+
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method that takes mandatory email and password string arguments
+            and returns a User object.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hashed_password = Auth._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+
+def __init__(self):
+        self._db = DB()
