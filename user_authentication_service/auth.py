6cca0521b4659bb248c0643d54f0618a8f580449
F-O-P <tayler.coon@gmail.com>
2024-01-23 17:55:08 -0600

    trying static method again

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
 """ Auth module """
-import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-
-
-def _hash_password(password: str) -> bytes:
-    """ Method that takes in a password string arguments and returns bytes.
-    """
-    password_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    return hashed_password
+import bcrypt
 
 
 class Auth:
@@ -21,7 +12,17 @@ class Auth:
     """
 
     def __init__(self):
+        """ Instance of the Auth class """
         self._db = DB()
+
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """ Method that takes in a password string arguments and returns bytes.
+        """
+        password_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password_bytes, salt)
+        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """ Method that takes mandatory email and password string arguments
@@ -32,6 +33,5 @@ class Auth:
             if user:
                 raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
-            hashed_password = Auth._hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
