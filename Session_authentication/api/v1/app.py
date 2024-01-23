68d7496d0879a89b72516b78e0b8f34175c79184
F-O-P <tayler.coon@gmail.com>
2024-01-23 11:11:00 -0600

    update to before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,13 +35,16 @@ def before_request_func() -> str:
         return
     if auth.require_auth(request.path, ['/api/v1/status/',
                                         '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
-            abort(401)
-
-        request.current_user = auth.current_user(request)
-        if auth.current_user(request) is None:
-            abort(403)
+                                        '/api/v1/forbidden/',
+                                        '/api/v1/auth_session/login/']):
+        if auth.require_auth(request.path, excluded_paths):
+            """ Check if authorization header and session cookie are none """
+            if auth.authorization_header(request) is None and\
+                    auth.session_cookie(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
