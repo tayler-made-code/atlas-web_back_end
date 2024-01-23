62f9995ac36f819f034b78d7680f813c0de8888a
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:50:14 -0600

    removed redundant code

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -30,9 +30,7 @@ class Auth:
     def _hash_password(password: str) -> bytes:
         """ Method that takes in a password string arguments and returns bytes.
         """
-        if isinstance(password, str):
-            password = password.encode('utf-8')
-
+        password_bytes = password.encode('utf-8')
         salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password, salt)
+        hashed_password = bcrypt.hashpw(password_bytes, salt)
         return hashed_password
