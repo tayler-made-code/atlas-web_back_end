75b79f0f3119a20e52791f422049b0747a4e4b87
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:25:35 -0600

    hash password implemented

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+def _hash_password(password):
+    if isinstance(password, str):
+        password = password.encode('utf-8')
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password, salt)
+    return hashed_password
