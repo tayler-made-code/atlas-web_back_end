fa7fedad31d8b2cac8204232590e5cae185aed44
F-O-P <tayler.coon@gmail.com>
2024-01-23 11:27:25 -0600

    trying to fix 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,26 +2,16 @@
 
 """ Session Auth """
 
-import os
 from api.v1.auth.auth import Auth
-import uuid
+import os
+
+auth_type = os.getenv('AUTH_TYPE')
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+else:
+    auth = Auth()
 
 
 class SessionAuth(Auth):
     """ user id by session id class """
-
-    user_id_by_session_id = {}
-
-    def create_session(self, user_id: str = None) -> str:
-        """ create session """
-        if user_id is None or not isinstance(user_id, str):
-            return None
-        session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ user id for session id """
-        if session_id is None or not isinstance(session_id, str):
-            return None
-        return self.user_id_by_session_id.get(session_id)
+    pass
