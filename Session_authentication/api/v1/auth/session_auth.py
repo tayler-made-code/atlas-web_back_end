bcaab1246b3423b0150b5520a1a734ffd5365d52
F-O-P <tayler.coon@gmail.com>
2024-01-23 12:15:55 -0600

    added current user method to SessionAuth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,10 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ current user """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        from models.user import User
+        return User.get(user_id)
