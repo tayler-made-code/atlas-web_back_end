a8b824390bdb443c65ddaf3cd7a43d41df3c9356
F-O-P <tayler.coon@gmail.com>
2024-01-23 14:59:35 -0600

    updated session info

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -33,7 +33,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Method that saves a new user to the database
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        new_user = User(email=email,
+                        hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
         return None
