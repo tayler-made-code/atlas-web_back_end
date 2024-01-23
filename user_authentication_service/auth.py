5bacdcefcf40ae703b69efb6df47ce98ed8d5ee2
F-O-P <tayler.coon@gmail.com>
2024-01-23 16:28:21 -0600

    fixed annotation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+""" Auth module """
 import bcrypt
 
 def _hash_password(password):
+    """ Method that takes in a password string arguments and returns bytes.
+    """
     if isinstance(password, str):
         password = password.encode('utf-8')
 
