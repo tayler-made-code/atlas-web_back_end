ea302ae3d22c1eeb2c7ddf0c486b3de16a03dc74
F-O-P <tayler.coon@gmail.com>
2024-01-23 20:00:08 -0600

    implemented create session method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,3 +60,16 @@ class Auth:
             pass
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """ Method that takes an email string argument and returns the session
+            ID as a string.
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError('User {} doesn\'t exist'.format(email))
+        
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
