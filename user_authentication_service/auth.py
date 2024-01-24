41e5d4a899c35dca5b8b1a50e8efba2b33fa9546
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:34:08 -0600

    implementing get_reset_password_token method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -98,3 +98,18 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Method that takes an email string argument and returns a string.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                raise ValueError
+
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+
+        except NoResultFound:
+            raise ValueError
