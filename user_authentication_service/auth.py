9edcd7c7a3874318889aa63d2c03eecba82ee779
F-O-P <tayler.coon@gmail.com>
2024-01-23 19:48:47 -0600

    updated to hashed_password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -44,8 +44,8 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if not user:
                 return False
-            
-            if bcrypt.checkpw(password.encode('utf-8'), user.password):
+
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
                 return True
 
         except NoResultFound:
