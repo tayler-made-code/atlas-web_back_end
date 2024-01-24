1522e7516358ad5cbb8cb1e6f42824fe0cb431fc
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:15:16 -0600

    implemented destroy_session method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,16 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Method that takes a single user_id integer argument and returns
+            None.
+        """
+        if user_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            return None
