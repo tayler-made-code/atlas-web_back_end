56eaffd457171e630ba1bef2c7ecb81c479aed6f
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:13:22 -0600

    implemented get_user_from_session_id method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -73,3 +73,15 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Method that takes a single session_id string argument and returns
+            the corresponding User or None """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
