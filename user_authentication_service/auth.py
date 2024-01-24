7412b7912c457aeaf43ce1b0e4df6ab0e911f9c0
F-O-P <tayler.coon@gmail.com>
2024-01-23 19:39:48 -0600

    added valid login method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,3 +35,20 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Method that takes email and password string arguments and reutnrs
+            a boolean.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                return False
+            
+            if bcrypt.checkpw(password.encode('utf-8'), user.password):
+                return True
+
+        except NoResultFound:
+            pass
+
+        return False
