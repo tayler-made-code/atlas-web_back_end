fea1bd551cf67ceb7c378c4dc7e9130d433a7170
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:56:54 -0600

    fixed variable name

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -97,9 +97,9 @@ def new_password():
     """ Method that updates the password """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
-    password = request.form.get('hashed_password')
+    new_password = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token, password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({
             'email': email,
             'message': 'Password updated'
