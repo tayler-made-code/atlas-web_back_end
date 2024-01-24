8482f3191e3394aa5400230b5ef7d16d9edc5add
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:36:32 -0600

    implemented password reset token function

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -73,5 +73,18 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({
+            'email': email,
+            'reset_token': reset_token
+        }), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
