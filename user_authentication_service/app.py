771c81f50a30d822c5f499e22bbbcefef6846216
F-O-P <tayler.coon@gmail.com>
2024-01-24 12:43:26 -0600

    implemented new password function

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -86,5 +86,20 @@ def reset_password():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def new_password():
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    password = request.form.get('password')
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({
+            'email': email,
+            'message': 'Password updated'
+        }), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
