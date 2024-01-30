fb6c9fd90432209719c748e00b2271409a14f6f4
F-O-P <tayler.coon@gmail.com>
2024-01-29 19:24:19 -0600

    task 4

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-"""  """
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class for testing GithubOrgClient """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, test_org_name, mock_get_json):
+        """ Test GithubOrgClient.org method """
+
+        """ Create a mock response """
+        mock_response = Mock()
+        mock_response.json.return_value = {'login': org}
+
+        """ patch get_json to return mock response """
+        mock_get_json.return_value = mock_response
+
+        """ create an instance of GithubOrgClient """
+        github_client = GithubOrgClient()
