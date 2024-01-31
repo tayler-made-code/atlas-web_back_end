c15cfb20bf0b3788c49b8954b9a6dfa0f108cd9f
F-O-P <tayler.coon@gmail.com>
2024-01-31 14:42:49 -0600

    spacing is now correct

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -22,7 +22,7 @@ babel = Babel(app)
 def get_locale():
     """ determine the best match with supported languages """
     if 'locale' in request.args and \
-      request.args['locale'] in app.config['LANGUAGES']:
+            request.args['locale'] in app.config['LANGUAGES']:
         return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
