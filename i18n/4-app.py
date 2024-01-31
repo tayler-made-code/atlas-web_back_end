f2bc5a000d7d78634e6bb8ea88d9e2a5aef93c48
F-O-P <tayler.coon@gmail.com>
2024-01-31 14:40:09 -0600

    fixing indentation

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -22,8 +22,8 @@ babel = Babel(app)
 def get_locale():
     """ determine the best match with supported languages """
     if 'locale' in request.args and \
-        request.args['locale'] in app.config['LANGUAGES']:
-      return request.args['locale']
+      request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
