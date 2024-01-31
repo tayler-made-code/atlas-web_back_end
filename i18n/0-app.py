8151cc699a813310cfc156be7bbec78f1b23a94c
F-O-P <tayler.coon@gmail.com>
2024-01-31 09:39:12 -0600

    fixed styling

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-""" setup a basic Flask app in 0-app.py. Create a single / route and an 
+""" setup a basic Flask app in 0-app.py. Create a single / route and an
     index.html template that simply outputs “Welcome to Holberton” as
     page title (<title>) and “Hello world” as header (<h1>) """
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+""" Create a single / route and an index.html template that simply outputs """
+@app.route('/')
 def hello_world():
-    """ outputs “Welcome to Holberton” as page title (<title>) and “Hello world”
-        as header (<h1>) """
+    """ outputs “Welcome to Holberton” as page title (<title>)
+      and “Hello world" as header (<h1>) """
     title = "Welcome to Holberton"
     header = "Hello world"
     return render_template('0-index.html', title=title, header=header)
