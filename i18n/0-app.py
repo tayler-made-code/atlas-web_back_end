68e42ff645a2fe1acc5c6a6e605c8325d804cdf1
F-O-P <tayler.coon@gmail.com>
2024-01-31 09:31:57 -0600

    added shebang and updated html page

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """ setup a basic Flask app in 0-app.py. Create a single / route and an 
     index.html template that simply outputs “Welcome to Holberton” as
     page title (<title>) and “Hello world” as header (<h1>) """
