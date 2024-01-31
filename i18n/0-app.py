184655299d0f517f5fdb1afeecdbe4073a58782a
F-O-P <tayler.coon@gmail.com>
2024-01-31 11:13:40 -0600

    1. Basic Babel setup

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-""" setup a basic Flask app in 0-app.py. Create a single / route and an
-    index.html template that simply outputs “Welcome to Holberton” as
-    page title (<title>) and “Hello world” as header (<h1>) """
+""" basic Flask app in 0-app.py """
 
 from flask import Flask, render_template
 
@@ -11,8 +9,5 @@ app = Flask(__name__)
 """ Create a single / route and an index.html template that simply outputs """
 @app.route('/')
 def hello_world():
-    """ outputs “Welcome to Holberton” as page title (<title>)
-      and “Hello world" as header (<h1>) """
-    title = "Welcome to Holberton"
-    header = "Hello world"
-    return render_template('0-index.html', title=title, header=header)
+    """ render a template """
+    return render_template('0-index.html')
