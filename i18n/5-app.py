0b2fe72c2e125b073abc792902573ce599eb853b
F-O-P <tayler.coon@gmail.com>
2024-01-31 14:53:28 -0600

    Mock logging in

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -12,12 +12,6 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -32,8 +26,26 @@ def get_locale():
         return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
 @app.route('/')
 def hello_world():
     """ render a template """
     return render_template('4-index.html')
+
+def get_user(user_id):
+    """ returns a user dictionary or None if ID cannot be found """
+    users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    }
+    return users.get(int(user_id))
+
+@app.before_request
+def before_request():
+    """ """
+    user_id = request.args.get('login_as', None)
+    if user_id:
+        flask.g.user = get_user(user_id)
+    else:
+        flask.g.user = None
