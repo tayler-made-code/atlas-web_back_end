944be8cb03ff61024bbbd4e9a386e090fecf9b25
F-O-P <tayler.coon@gmail.com>
2024-01-31 12:28:33 -0600

    reoganizizing

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -8,16 +8,15 @@ from flask_babel import Babel
 class Config(object):
     """Config class for Babel"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app = Flask(__name__)
-
-app.config["BABEL_DEFAULT_LOCALE"] = "en"
-app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
-
 app.config.from_object(Config)
 babel = Babel(app)
 
-""" Create a single / route and an index.html template that simply outputs """
+""" Create a single / route and an index.html template
+    that simply outputs """
 @app.route('/')
 def hello_world():
     """ render a template """
