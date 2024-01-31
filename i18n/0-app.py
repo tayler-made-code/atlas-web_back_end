971595c7f6a754b4f530bc5c62bcd228913e51f6
F-O-P <tayler.coon@gmail.com>
2024-01-31 09:24:12 -0600

    adding requirements

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -8,5 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """ return a simple string for starters """
-    return 'Welcome to Holberton'
+    """ outputs “Welcome to Holberton” as page title (<title>) and “Hello world”
+        as header (<h1>) """
+    title = "Welcome to Holberton"
+    header = "Hello world"
+    return render_template('0-index.html', title=title, header=header)
