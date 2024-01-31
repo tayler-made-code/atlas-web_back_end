4b99eb9495b5566a0df1a85bc0f21b1846c32fae
F-O-P <tayler.coon@gmail.com>
2024-01-31 14:17:11 -0600

    forgot to add html

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -20,8 +20,10 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """ determine the best match with supported languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+  """ determine the best match with supported languages """
+  if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+    return request.args['locale']
+  return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
