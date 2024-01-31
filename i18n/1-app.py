948083cb99415ae5b0ce8f2afd74dc6e999c48e7
F-O-P <tayler.coon@gmail.com>
2024-01-31 12:16:45 -0600

    1. Basic Babel setup

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
-""" basic Flask app in 0-app.py """
+""" basic Flask app in 1-app.py """
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
+
+class Config(object):
+    """Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
 
 app = Flask(__name__)
 babel = Babel(app)
+
+app.config[BABEL_DEFAULT_LOCALE] = 'en'
+app.config[BABEL_DEFAULT_TIMEZONE] = 'UTC'
+
+app.config.from_object(Config)
 
 """ Create a single / route and an index.html template that simply outputs """
 @app.route('/')
 def hello_world():
     """ render a template """
     return render_template('1-index.html')
-
-babel = Babel(app)
-class Config(object):
-    """Config class for Babel"""
-    LANGUAGES = ["en", "fr"]
-
-app.config.from_object('1-app.Config')
