b47f7d5bbcc83afe9808b1148870be87774d6436
F-O-P <tayler.coon@gmail.com>
2024-02-10 22:12:49 -0600

    Solution needs to be set to specific year

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Assuming the table name is 'metal_bands' and it has columns 'band_name' and 'lifespan'
 -- You should use attributes formed and split for computing the lifespan or the Glam Rock bands
-SELECT band_name, (IFNULL(split, CURRENT_DATE()) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam Rock%'
 ORDER BY lifespan DESC;
