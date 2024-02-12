0c311ea5e2008c62d3aa41e27cdd2c55e4f1c24f
F-O-P <tayler.coon@gmail.com>
2024-02-12 17:44:22 -0600

    Optimize simple search

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -4,23 +4,11 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
     DECLARE average_score FLOAT;
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
+    
+    -- Compute the average score
+    SELECT AVG(score) INTO average_score FROM correction WHERE corrections.user_id = p_user_id;
 
-    -- compute the total score
-    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = p_user_id;
-
-    -- compute the total projects
-    SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = p_user_id;
-
-    IF total_projects > 0 THEN
-        --compute the average score
-        SET average_score = total_score / total_projects;
-    ELSE
-        SET average_score = 0;
-    END IF;
-
-    -- update the user average score
+    -- Update the user average score
     UPDATE users SET average_score = average_score WHERE id = p_user_id;
 END//
 DELIMITER ;
