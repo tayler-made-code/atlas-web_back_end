fd72b86270004002fce452c4d4a42b2c683ad395
F-O-P <tayler.coon@gmail.com>
2024-02-13 11:40:48 -0600

    Average score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,14 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes
 -- and store the average score for a student. An average score can be a decimal
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE average_score FLOAT;
     
     -- Compute the average score
-    SELECT AVG(score) INTO average_score FROM correction WHERE corrections.user_id = p_user_id;
+    SELECT AVG(score) INTO average_score FROM corrections WHERE corrections.user_id = user_id;
 
     -- Update the user average score
-    UPDATE users SET average_score = average_score WHERE id = p_user_id;
+    UPDATE users SET average_score = average_score WHERE id = user_id;
 END//
 DELIMITER ;
