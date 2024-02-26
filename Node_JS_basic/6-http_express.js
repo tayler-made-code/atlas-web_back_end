1184b6f4412f87c2da804193f54f4003e50d769b
F-O-P <tayler.coon@gmail.com>
2024-02-26 15:54:03 -0600

    Create a small HTTP server using Express

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,6 +1,7 @@
-// Install Express and in a file named 6-http_express.js, create a small HTTP server using Express module
+// Create a small HTTP server using Express module
 
 const express = require('express');
+
 const app = express(); // It should be assigned to the variable app
 
 // Displays "Hello Holberton School!" in the page body for the endpoint /
