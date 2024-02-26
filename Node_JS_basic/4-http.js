da5b2f5975c5b851ce5b3f4dc4b1390a56e6e0c8
F-O-P <tayler.coon@gmail.com>
2024-02-25 21:58:57 -0600

    Create a small HTTP server using Node's HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,7 +4,6 @@ var http = require('http');
 
 // the server should be assigned to the variable app
 var app = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
   // Displays Hello Holberton School! in the page body
   res.write('Hello Holberton School!');
   res.end();
