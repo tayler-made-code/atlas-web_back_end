fa3d3ca2edd553b93e020b6bb17b1ca059a47e7c
F-O-P <tayler.coon@gmail.com>
2024-02-25 21:37:59 -0600

    Reading a file asynchronously with Node JS

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+function countStudents(filePath) {
+  // Function that counts the number of students in a database
+  // and logs them to the console
+  const fullPath = path.resolve(filePath);
+  
+  return new Promise((resolve, reject) => {
+    fs.readFile(fullPath, 'utf-8')
+      .then(data => {
+        // Split the data by lines
+        const lines = data.split('\n');
+        
+        // Remove any empty lines
+        const usedLines = lines.filter((line) => line.length > 0);
+        
+        // Remove header
+        const students = usedLines.slice(1);
+        
+        // Log the number of students
+        console.log(`Number of students: ${students.length}`);
+        
+        // Log the number of students in each field
+        const fields = {};
+        for (const student of students) {
+          const field = student.split(',')[3];
+          if (fields[field]) {
+            fields[field].push(student.split(',')[0]);
+          } else {
+            fields[field] = [student.split(',')[0]];
+          }
+        }
+        for (const field in fields) {
+          if (field) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
+        }
+        resolve();
+      })
+      .catch(error => {
+        reject(Error('Cannot load the database'));
+      });
+    });
+}
+
+module.exports = countStudents;
