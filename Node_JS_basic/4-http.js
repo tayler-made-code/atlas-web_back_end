4866c2168d592c593ba071fbe5aed7e9d99e9549
F-O-P <tayler.coon@gmail.com>
2024-02-25 21:53:47 -0600

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+// create a small server using http module
+var http = require('http');
+// Displays Hello Holberton School! in the page body for any endpoint as plain text
+
+// the server should be assigned to the variable app
+var app = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Displays Hello Holberton School! in the page body
+  res.end('Hello Holberton School!');
+});
+// HTTP server should listen on port 1245
+app.listen(1245);
