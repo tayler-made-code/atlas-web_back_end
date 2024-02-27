38554d2223afcae1de5de828b9bca6fb764802bc
F-O-P <tayler.coon@gmail.com>
2024-02-26 21:57:46 -0600

    added test case for first number rounded

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -12,5 +12,6 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
   });
 });
