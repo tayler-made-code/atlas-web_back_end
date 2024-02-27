0939999c4d439e9f446d76556c4733091370d1e7
F-O-P <tayler.coon@gmail.com>
2024-02-27 09:02:11 -0600

    overkill

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -9,15 +9,35 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return the sum of two numbers, with the first rounded', () => {
+  it('should return the sum of two numbers, with the first rounded up', () => {
+    assert.strictEqual(calculateNumber(1.6, 3), 5);
+  });
+
+  it('should return the sum of two numbers, with the first rounded down', () => {
     assert.strictEqual(calculateNumber(1.3, 3), 4);
   });
 
-  it('should return the sum of two numbers, with the second rounded', () => {
+  it('should return the sum of two numbers, with the second rounded up', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return the sum of two rounded numbers', () => {
+  it('should return the sum of two numbers, with the second rounded down', () => {
+    assert.strictEqual(calculateNumber(1, 3.3), 4);
+  });
+
+  it('should return the sum of two numbers rounded down', () => {
+    assert.strictEqual(calculateNumber(1.3, 3.4), 4);
+  });
+
+  it('should return the sum of two numbers rounded up', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return the sum of two numbers rounded down and up', () => {
     assert.strictEqual(calculateNumber(1.3, 3.7), 5);
+  });
+
+  it('should return the sum of two numbers rounded up and down', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.3), 5);
   });
 });
