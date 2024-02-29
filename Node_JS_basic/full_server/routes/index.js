8328ac08ab102a3965655a49958261acb26ebc83
F-O-P <tayler.coon@gmail.com>
2024-02-28 19:43:29 -0600

    fixed some paths, names and code

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,12 +1,9 @@
 // import the AppController and StudentController
 import AppController from '../controllers/AppController';
-import StudentController from '../controllers/StudentController';
+import StudentsController from '../controllers/StudentsController';
 
 // Import Express
 const express = require('express');
-
-// Create an Express application
-const app = express();
 
 // Use the router
 const router = express.Router();
@@ -15,10 +12,10 @@ const router = express.Router();
 router.get('/', AppController.getHomepage);
 
 // define route for the students
-router.get('/students', StudentController.getAllStudents);
+router.get('/students', StudentsController.getAllStudents);
 
 // define router for studetns by major
-router.get('/students/:major', StudentController.getAllStudentsByMajor);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 // Export the router
 module.exports = router;
